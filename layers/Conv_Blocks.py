96b4183c989d3d77c65e20febd47a7027d436da9
Xiang Xian, Zhang <j087061439@gmail.com>
2024-03-12 15:38:40 +0800

    test: resnet

--- layers/Conv_Blocks.py
+++ layers/Conv_Blocks.py
@@ -135,14 +135,20 @@ class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size//2)
+        self.conv2 = nn.Conv2d(out_channels, out_channels*2, kernel_size=kernel_size, padding=kernel_size//2)
+        self.conv3 = nn.Conv2d(out_channels*2, out_channels, kernel_size=kernel_size, padding=kernel_size//2)
+        self.conv4 = nn.Conv2d(out_channels, in_channels, kernel_size=kernel_size, padding=kernel_size//2)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, in_channels, kernel_size=kernel_size, padding=kernel_size//2)
 
     def forward(self, x):
         identity = x
         out = self.conv1(x)
         out = self.relu(out)
         out = self.conv2(out)
+        out = self.conv3(x)
+        out = self.relu(out)
+        out = self.conv4(out)
+
         out += identity
         out = self.relu(out)
         return out
--- models/TimesNet.py
+++ models/TimesNet.py
@@ -34,17 +34,17 @@ class TimesBlock(nn.Module):
         # )
 
         # resnet
-        # self.conv = nn.Sequential(
-        #     ResidualBlock(in_channels=configs.d_model, out_channels=512),
-        #     ResidualBlock(in_channels=configs.d_ff, out_channels=256),
-        #     ResidualBlock(in_channels=configs.d_ff, out_channels=128),
-        #     ResidualBlock(in_channels=configs.d_ff, out_channels=64),
-        # )
-
         self.conv = nn.Sequential(
-            ConvNeXtBlock(in_channels=configs.d_model, out_channels=configs.d_ff), 
-            ConvNeXtBlock(in_channels=configs.d_ff, out_channels=configs.d_model), 
+            ResidualBlock(in_channels=configs.d_model, out_channels=512),
+            ResidualBlock(in_channels=configs.d_ff, out_channels=256),
+            ResidualBlock(in_channels=configs.d_ff, out_channels=128),
+            ResidualBlock(in_channels=configs.d_ff, out_channels=64),
         )
+
+        # self.conv = nn.Sequential(
+        #     ConvNeXtBlock(in_channels=configs.d_model, out_channels=configs.d_ff), 
+        #     ConvNeXtBlock(in_channels=configs.d_ff, out_channels=configs.d_model), 
+        # )
 
         # resnet18
         # self.conv = ResNet(ResBlock)
