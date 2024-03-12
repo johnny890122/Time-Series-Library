3b3c93bb90b38aff3c43e736adf97601f52a8700
Xiang Xian, Zhang <j087061439@gmail.com>
2024-03-12 10:44:34 +0800

    feat: run backbone: inception v1

--- models/TimesNet.py
+++ models/TimesNet.py
@@ -25,20 +25,20 @@ class TimesBlock(nn.Module):
         self.pred_len = configs.pred_len
         self.k = configs.top_k
         # parameter-efficient design
-        # self.conv = nn.Sequential(
-        #     Inception_Block_V2(configs.d_model, configs.d_ff,
-        #                        num_kernels=configs.num_kernels),
-        #     nn.GELU(),
-        #     Inception_Block_V2(configs.d_ff, configs.d_model,
-        #                        num_kernels=configs.num_kernels)
-        # )
-
         self.conv = nn.Sequential(
-            ResidualBlock(in_channels=configs.d_model, out_channels=configs.d_ff),
-            ResidualBlock(in_channels=configs.d_ff, out_channels=configs.d_ff),
-            ResidualBlock(in_channels=configs.d_ff, out_channels=configs.d_ff),
-            ResidualBlock(in_channels=configs.d_ff, out_channels=configs.d_model),
+            Inception_Block_V1(configs.d_model, configs.d_ff,
+                               num_kernels=configs.num_kernels),
+            nn.GELU(),
+            Inception_Block_V1(configs.d_ff, configs.d_model,
+                               num_kernels=configs.num_kernels)
         )
+
+        # self.conv = nn.Sequential(
+        #     ResidualBlock(in_channels=configs.d_model, out_channels=configs.d_ff),
+        #     ResidualBlock(in_channels=configs.d_ff, out_channels=configs.d_ff),
+        #     ResidualBlock(in_channels=configs.d_ff, out_channels=configs.d_ff),
+        #     ResidualBlock(in_channels=configs.d_ff, out_channels=configs.d_model),
+        # )
 
     def forward(self, x):
         B, T, N = x.size()
